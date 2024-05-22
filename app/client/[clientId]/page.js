daced6e611e47162a7eeb7e59b3020b51d399ad7
GarryFirst-cripto <vist_01@ukr.net>
2024-05-22 12:49:39 +0300

    state_00

--- app/client/[clientId]/page.js
+++
@@ -1,8 +0,0 @@
-export default function clientId({ params }) {
-  return (
-    <div style={{ width: '25%', display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
-      <h2>Id Id Id Page </h2>
-      <h2>{ params.clientId }</h2>      
-    </div>
-  )
-}
