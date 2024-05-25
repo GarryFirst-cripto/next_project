888e7a9a670c08c89f2f592d35b897b3efe10870
GarryFirst-cripto <vist_01@ukr.net>
2024-05-25 18:52:08 +0300

    stage_new

---
+++ app/about/(info)/key/page.js
@@ -0,0 +1,11 @@
+const page = () => {
+  return (
+    <div>
+      KEY KEY KEY KEY
+      <br />
+      KEY KEY KEY KEY
+    </div>
+  )
+}
+
+export default page
---
+++ app/about/(info)/use/page.js
@@ -0,0 +1,10 @@
+
+const page = () => {
+  return (
+    <div>
+      INFO INFO INFO
+    </div>
+  )
+}
+
+export default page
