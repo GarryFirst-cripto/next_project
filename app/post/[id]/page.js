e5ea97b121a58c2a34c53f4360cb8749f319b206
GarryFirst-cripto <vist_01@ukr.net>
2024-06-05 13:42:47 +0300

    state_02_localStore_fix

--- app/post/[id]/page.js
+++ app/post/[id]/page.js
@@ -2,16 +2,16 @@
 
 import { useEffect } from 'react';
 import { useMyContext } from '../../provider/context';
-import { useRouter } from 'next/navigation';
+// import { useRouter } from 'next/navigation';
 import Post from '@/app/components/Postinfo';
 
 const Postpage = ({ params }) => {
   const { value, setValue } = useMyContext();
-  const router = useRouter();
+  // const router = useRouter();
 
   useEffect(() => {
     const data = localStorage.getItem('pageData');
-    if (data) setValue(JSON.parse(data))
+    if (!value && data) setValue(JSON.parse(data))
   }, []);
   useEffect(() => {
     if (value) localStorage.setItem('pageData', JSON.stringify(value));
